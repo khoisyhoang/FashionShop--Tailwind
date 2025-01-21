f412594fef0619c06ae92e70db2fd6635a23f4c3
potatoeshoang <khoisyhoang@gmail.com>
2025-01-21 13:32:02 -0500

    Project 3

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          main: '#000000',
+        }
+      },
+      screens: {
+        'sm': '576px', 
+        'md': '768px', 
+        'lg': '992px',
+        'xl': '1272px',
+        '2xl': '1272px', 
+      }
+    }
+  }
